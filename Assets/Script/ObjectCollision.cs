0f9a766a244fc53cbebd5ce9b4e84a2fc3e5fd53
RizaHarunEffendi <rizaharun26@gmail.com>
2020-04-14 02:41:39 +0700

    pertemuan 7

--- Assets/Script/ObjectCollision.cs
+++ Assets/Script/ObjectCollision.cs
@@ -4,32 +4,17 @@ using UnityEngine;
 
 public class ObjectCollision : MonoBehaviour {
 
-	public AudioSource Musuh1;
-	public AudioSource Musuh2;
-	public AudioSource Musuh3;
-	public AudioSource Musuh4;
+	// public AudioSource Musuh1;
+	// public AudioSource Musuh2;
+	// public AudioSource Musuh3;
+	// public AudioSource Musuh4;
 		
 	// Use this for initialization
 	void OnCollisionEnter(Collision col) {
 		if (col.gameObject.tag == "ObjectMusuh"){
-			Debug.Log("Collide Musuh1!");
-			Musuh1.GetComponent<AudioSource> ();
-			Musuh1.Play ();
-			col.gameObject.SetActive (false);
-		}if (col.gameObject.tag == "ObjectMusuh2"){
-			Debug.Log("Collide Musuh2!");
-			Musuh2.GetComponent<AudioSource> ();
-			Musuh2.Play ();
-			col.gameObject.SetActive (false);
-		}if (col.gameObject.tag == "ObjectMusuh3"){
-			Debug.Log("Collide Musuh3!");
-			Musuh3.GetComponent<AudioSource> ();
-			Musuh3.Play ();
-			col.gameObject.SetActive (false);
-		}if (col.gameObject.tag == "ObjectMusuh4"){
-			Debug.Log("Collide Musuh4!");
-			Musuh4.GetComponent<AudioSource> ();
-			Musuh4.Play ();
+			Debug.Log("Collide!");
+			AudioSource aud = gameObject.GetComponent<AudioSource> ();
+			aud.Play ();
 			col.gameObject.SetActive (false);
 		}
 	}
