07564872c119d15c7ff9fa45180502b6835d73f8
RizaHarunEffendi <rizaharun26@gmail.com>
2020-07-18 11:28:55 +0700

    pertemuan 10

--- Assets/Script/Health.cs
+++ Assets/Script/Health.cs
@@ -6,15 +6,28 @@ public class Health : MonoBehaviour {
 	[SerializeField]
 	int _maximumHealth = 100;
 	int _currentHealth = 0;
+	Renderer _renderer;
+
+	public bool isDeath(){
+		return _currentHealth <= 0;
+	}
+
+	PlayerStats _playerStats;
 	// Use this for initialization
 	void Start () {
+		_renderer = GetComponentInChildren<Renderer> ();
 		_currentHealth = _maximumHealth;
+
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+		_playerStats = player.GetComponent<PlayerStats>();
 		// GetComponent<Health> ().Damage (100);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (isDeath() && !_renderer.isVisible){
+			Destroy (gameObject);
+		}
 	}
 
 	public void Damage(int damageValue){
@@ -23,9 +36,15 @@ public class Health : MonoBehaviour {
 		{
 			Animation anim = GetComponentInChildren<Animation> ();
 			anim.Stop ();
-			// Destroy (GetComponent<PlayerMovement> ());
-			// Destroy (GetComponent<PlayerAnimation> ());
+			
+			_playerStats.ZombieKilled++;
+			EnemySpawnManager.onEnemyDeath ();
+			Destroy (GetComponent<EnemyMovement>());
+			Destroy (GetComponent<EnemyAttack>());
+			Destroy (GetComponent<CharacterController>());
+			Destroy (gameObject, 8.0f);
 
+			EnemySpawnManager.onEnemyDeath ();
 			Destroy (GetComponent<EnemyMovement> ());
 			Destroy (GetComponent<CharacterController> ());
 
